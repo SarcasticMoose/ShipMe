9af897c92f54c5e2535a4cc91ae58d022a07b671
Jakub Tokarczyk <101338588+SarcasticMoose@users.noreply.github.com>
2025-11-02 01:59:12 +0100

    Added API versioning (#2)
    feat: added Carter library for logic separation of endpoints
    feat: added api versioning
    feat: added example endpoint

---
+++ App/ShipMe.Api/Setup.cs
@@ -0,0 +1,19 @@
+using Asp.Versioning;
+
+namespace ShipMe.Api;
+
+public static class Setup
+{
+    public static void AddVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+        {
+            options.DefaultApiVersion = new ApiVersion(1);
+            options.ApiVersionReader = new UrlSegmentApiVersionReader();
+        }).AddApiExplorer(options =>
+        {
+            options.GroupNameFormat = "'v'V";
+            options.SubstituteApiVersionInUrl = true;
+        });
+    }
+}
